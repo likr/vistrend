157ac5e193b6a496e202b0d39bf0d9c9483a5638
Yosuke ONOUE <onoue@likr-lab.com>
2013-10-21 11:55:16 +0900

    Update data

--- py/collect.py
+++ py/collect.py
@@ -8,12 +8,15 @@ import csv
 def main():
     root = os.path.dirname(__file__) + '/../'
     infilenames = [
+        'orig/Evaluation Method - 2008info.csv',
+        'orig/Evaluation Method - 2009info.csv',
         'orig/Evaluation Method - 2010sci.csv',
         'orig/Evaluation Method - 2010info.csv',
         'orig/Evaluation Method - 2010vast.csv',
         'orig/Evaluation Method - 2011sci.csv',
         'orig/Evaluation Method - 2011info.csv',
         'orig/Evaluation Method - 2011vast.csv',
+        'orig/Evaluation Method - 2012sci.csv',
         'orig/Evaluation Method - 2012info.csv',
         'orig/Evaluation Method - 2012vast.csv',
     ]
