dc2e1a346c90f4576004c92431c4f4f7c14abfc0
Yosuke ONOUE <onoue@likr-lab.com>
2013-10-24 12:23:20 +0900

    Update data

--- py/collect.py
+++ py/collect.py
@@ -8,8 +8,22 @@ import csv
 def main():
     root = os.path.dirname(__file__) + '/../'
     infilenames = [
+        #'orig/Evaluation Method - 2004sci.csv',
+        #'orig/Evaluation Method - 2004info.csv',
+        #'orig/Evaluation Method - 2005sci.csv',
+        #'orig/Evaluation Method - 2005info.csv',
+        'orig/Evaluation Method - 2006sci.csv',
+        'orig/Evaluation Method - 2006info.csv',
+        'orig/Evaluation Method - 2006vast.csv',
+        'orig/Evaluation Method - 2007sci.csv',
+        'orig/Evaluation Method - 2007info.csv',
+        'orig/Evaluation Method - 2007vast.csv',
+        'orig/Evaluation Method - 2008sci.csv',
         'orig/Evaluation Method - 2008info.csv',
+        'orig/Evaluation Method - 2008vast.csv',
+        'orig/Evaluation Method - 2009sci.csv',
         'orig/Evaluation Method - 2009info.csv',
+        'orig/Evaluation Method - 2009vast.csv',
         'orig/Evaluation Method - 2010sci.csv',
         'orig/Evaluation Method - 2010info.csv',
         'orig/Evaluation Method - 2010vast.csv',
@@ -19,6 +33,9 @@ def main():
         'orig/Evaluation Method - 2012sci.csv',
         'orig/Evaluation Method - 2012info.csv',
         'orig/Evaluation Method - 2012vast.csv',
+        'orig/Evaluation Method - 2013sci.csv',
+        'orig/Evaluation Method - 2013info.csv',
+        'orig/Evaluation Method - 2013vast.csv',
     ]
     outfilename = root + 'vistrend.csv'
     writer = csv.writer(open(outfilename, 'w'))
